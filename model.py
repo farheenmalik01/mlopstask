9013cdae1056536874265b773807e245f64ed3c0
Farheen Malik <156839406+farheenmalik01@users.noreply.github.com>
2024-09-06 21:58:09 +0500

    model.py

--- model.py
+++ model.py
@@ -1,14 +1,11 @@
-# model.py
 import numpy as np
 from sklearn.linear_model import LinearRegression
 import joblib
 
-# Create a simple linear regression model
 X = np.array([[1], [2], [3], [4], [5]])
 y = np.array([2, 4, 6, 8, 10])
 
 model = LinearRegression()
 model.fit(X, y)
 
-# Save the trained model
 joblib.dump(model, 'model.pkl')
